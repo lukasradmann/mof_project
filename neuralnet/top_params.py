a6666b20ed773f6e4ad8c8c1c4b9e3307e456651
aluxf <alexfooladi@hotmail.com>
2025-01-03 12:20:31 +0100

    restructure project

---
+++ neuralnet/top_params.py
@@ -0,0 +1,39 @@
+# Hardcoded top 5 parameters for the neural network
+
+top_params = [{
+    "batch_size": 128,
+    "learning_rate": 0.0001239,
+    "n_layers": 3,
+    "n_units": 128,
+    "dropout": 0.10446
+},
+{
+    "batch_size": 192,
+    "learning_rate": 0.0012946,
+    "n_layers": 3,
+    "n_units": 128,
+    "dropout": 0.13115
+},
+{
+    "batch_size": 112,
+    "learning_rate": 0.001119,
+    "n_layers": 3,
+    "n_units": 128,
+    "dropout": 0.12337
+},
+{
+    "batch_size": 104,
+    "learning_rate": 0.0002346,
+    "n_layers": 3,
+    "n_units": 128,
+    "dropout": 0.16569
+},
+{
+    "batch_size": 112,
+    "learning_rate": 0.0003531,
+    "n_layers": 3,
+    "n_units": 128,
+    "dropout": 0.11978
+}
+]
+
